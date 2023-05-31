96ec4d063c853d08dfeee15e4b195de49408b91f
arnaudchampetier <arnaud.champetier9@gmail.com>
2023-05-31 17:56:27 +0200

    remonter le trait pour les navigateurs mac

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -128,7 +128,7 @@ function Header() {
                   onClick={() => handleClick("epicerie")}
                 >
                   <span
-                    className={`hidden 2xl:block  uppercase relative cursor-pointer transition-all duration-500 before:content-[''] before:absolute before:bottom-[-26px] before:left-1/2 before:-translate-x-1/2 before:w-0 before:h-1.5 before:rounded-full before:opacity-0 before:transition-all before:duration-500 before:bg-gradient-to-r before:from-gray-600 before:via-slate-400 before:to-zinc-500 hover:before:w-full hover:before:opacity-100 ${
+                    className={`hidden 2xl:block  uppercase relative cursor-pointer transition-all duration-500 before:content-[''] before:absolute before:bottom-[-10px] before:left-1/2 before:-translate-x-1/2 before:w-0 before:h-1.5 before:rounded-full before:opacity-0 before:transition-all before:duration-500 before:bg-gradient-to-r before:from-gray-600 before:via-slate-400 before:to-zinc-500 hover:before:w-full hover:before:opacity-100 ${
                       activeItem === "epicerie" ? "epicerie-fine active" : ""
                     }`}
                   >
@@ -155,7 +155,7 @@ function Header() {
                   onClick={() => handleClick("traiteur")}
                 >
                   <span
-                    className={`hidden 2xl:block hover:scale-105  uppercase relative cursor-pointer transition-all duration-500 before:content-[''] before:absolute before:bottom-[-26px] before:left-1/2 before:-translate-x-1/2 before:w-0 before:h-1.5 before:rounded-full before:opacity-0 before:transition-all before:duration-500 before:bg-gradient-to-r before:from-gray-600 before:via-slate-400 before:to-zinc-500 hover:before:w-full hover:before:opacity-100 ${
+                    className={`hidden 2xl:block hover:scale-105  uppercase relative cursor-pointer transition-all duration-500 before:content-[''] before:absolute before:bottom-[-10px] before:left-1/2 before:-translate-x-1/2 before:w-0 before:h-1.5 before:rounded-full before:opacity-0 before:transition-all before:duration-500 before:bg-gradient-to-r before:from-gray-600 before:via-slate-400 before:to-zinc-500 hover:before:w-full hover:before:opacity-100 ${
                       activeItem === "traiteur" ? "traiteur active" : ""
                     }`}
                   >
@@ -186,7 +186,7 @@ function Header() {
                   onClick={() => handleClick("petiterestauration")}
                 >
                   <span
-                    className={`hidden 2xl:block hover:scale-105  uppercase relative cursor-pointer transition-all duration-500 before:content-[''] before:absolute before:bottom-[-26px] before:left-1/2 before:-translate-x-1/2 before:w-0 before:h-1.5 before:rounded-full before:opacity-0 before:transition-all before:duration-500 before:bg-gradient-to-r before:from-gray-600 before:via-slate-400 before:to-zinc-500 hover:before:w-full hover:before:opacity-100 ${
+                    className={`hidden 2xl:block hover:scale-105  uppercase relative cursor-pointer transition-all duration-500 before:content-[''] before:absolute before:bottom-[-10px] before:left-1/2 before:-translate-x-1/2 before:w-0 before:h-1.5 before:rounded-full before:opacity-0 before:transition-all before:duration-500 before:bg-gradient-to-r before:from-gray-600 before:via-slate-400 before:to-zinc-500 hover:before:w-full hover:before:opacity-100 ${
                       activeItem === "petiterestauration"
                         ? "petiterestauration active"
                         : ""
@@ -217,7 +217,7 @@ function Header() {
                   onClick={() => handleClick("salon")}
                 >
                   <span
-                    className={`hidden 2xl:block hover:scale-105  uppercase relative cursor-pointer transition-all duration-500 before:content-[''] before:absolute before:bottom-[-26px] before:left-1/2 before:-translate-x-1/2 before:w-0 before:h-1.5 before:rounded-full before:opacity-0 before:transition-all before:duration-500 before:bg-gradient-to-r before:from-gray-600 before:via-slate-400 before:to-zinc-500 hover:before:w-full hover:before:opacity-100 ${
+                    className={`hidden 2xl:block hover:scale-105  uppercase relative cursor-pointer transition-all duration-500 before:content-[''] before:absolute before:bottom-[-10px] before:left-1/2 before:-translate-x-1/2 before:w-0 before:h-1.5 before:rounded-full before:opacity-0 before:transition-all before:duration-500 before:bg-gradient-to-r before:from-gray-600 before:via-slate-400 before:to-zinc-500 hover:before:w-full hover:before:opacity-100 ${
                       activeItem === "salon" ? "salon active" : ""
                     }`}
                   >
