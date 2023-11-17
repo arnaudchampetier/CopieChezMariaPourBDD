f68f9ea7091894c21830552cf5d697e49d0dd10c
arnaudchampetier <arnaud.champetier9@gmail.com>
2023-11-17 18:37:50 +0100

    ajustements responsive

--- src/components/ProductDescriptionModal.jsx
+++ src/components/ProductDescriptionModal.jsx
@@ -23,12 +23,12 @@ function ProductDescriptionModal({
 
   return (
     <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center z-50">
-      <div className="bg-gray-200 opacity-95 p-4 rounded-lg shadow-xl md:w-3/4 lg:w-1/2 xl:w-2/3 2xl:w-1/2 md:mx-3 mx-5 sm:h-4/5 2xl:h-3/4 flex flex-col relative sm:mt-44  ">
+      <div className="bg-gray-200 opacity-95 p-4 rounded-lg shadow-xl md:w-3/4 lg:w-1/2 xl:w-2/3 2xl:w-1/2 md:mx-3 mx-5 sm:h-4/5 2xl:h-3/4 flex flex-col relative mt-32  ">
         <button
           onClick={onClose}
-          className="absolute top-2 right-2 text-red-600 hover:text-red-800 focus:outline-none"
+          className="absolute top-2 right-2 text-red-600 hover:text-red-800 focus:outline-none cursor-pointer"
         >
-          <MdCancel size={24} />
+          <MdCancel size={30} />
         </button>
         <div className="w-full h-1/2 flex flex-col justify-center items-center relative">
           <img
@@ -75,7 +75,7 @@ function ProductDescriptionModal({
           disabled={product.isSoldOut}
           className={`${
             product.isSoldOut ? "bg-gray-300 cursor-not-allowed" : "bg-cyan-900"
-          } text-white px-4 py-2 mt-2 rounded hover:bg-purple-700 w-1/4 mx-auto`}
+          } text-white px-4 py-2 mt-2 rounded hover:bg-purple-700 md:w-1/4 mx-auto`}
         >
           {product.isSoldOut ? "En rupture" : "Ajouter au panier"}
         </button>
